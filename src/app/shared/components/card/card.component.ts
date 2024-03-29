09f72e4eda1b580801f2311803a6ef83c5a28742
mdjeee <i.sejtniyazov@mail.ru>
2024-03-29 14:19:17 +0500

    feat: remove pagination on resize

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ElementRef, Input, TemplateRef, ViewChild } from '@angular/core';
 import { fadeInOut } from '../../../animations/fade-in-out.animations';
 import { cardRotate } from '../../../animations/card.animations';
 
@@ -8,7 +8,7 @@ import { cardRotate } from '../../../animations/card.animations';
   styleUrl: './card.component.scss',
   animations: [fadeInOut, cardRotate]
 })
-export class CardComponent {
+export class CardComponent implements AfterViewInit {
   @Input() card: any = undefined;
 
   @ContentChild('cardTitleTemplate') cardTitle: TemplateRef<any>;
@@ -17,9 +17,17 @@ export class CardComponent {
   @ContentChild('cardChipsTemplate') cardChips: TemplateRef<any>;
   @ContentChild('cardCaptionTemplate') cardCaption: TemplateRef<any>;
 
+  @ViewChild('cardContainer') cardContainer: ElementRef;
+
   state: string = 'inactive'
+  cardHeight: number = 0;
 
   toggleCard() {
     this.state = (this.state == 'inactive') ? 'active' : 'inactive';
+  }
+
+  ngAfterViewInit() {
+    if(!this.cardHeight)
+    this.cardHeight = this.cardContainer.nativeElement.offsetHeight;
   }
 }
--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,18 +1,23 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { fromEvent, Subject, Subscription, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss'
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, OnDestroy {
   @Input() count: number | null;
   @Input() next: string | null | undefined;
   @Input() previous: string | null | undefined;
   @Input() loading: boolean | null = false;
   @Output() emitPage = new EventEmitter<number>();
-  currentPage: number | undefined;
+  public currentPage: number | undefined;
+  private isWindowSmall: boolean = false;
+  private resizeSubscription!: Subscription;
+  private destroyed = new Subject()
+
 
   constructor(
     private router: Router,
@@ -32,6 +37,9 @@ export class PaginationComponent implements OnInit {
   }
 
   createPages(count: number | null) {
+    if(this.isWindowSmall) {
+      return [];
+    }
     const pages = count ? Math.ceil(count/10) : 1;
     return Array(pages).fill(0);
   }
@@ -41,6 +49,19 @@ export class PaginationComponent implements OnInit {
       this.currentPage = parseInt(params['page'], 10) || 1;
       this.emitPage.emit(this.currentPage);
     })
+
+    this.resizeSubscription = fromEvent(window, 'resize')
+      .pipe(
+        takeUntil(this.destroyed)
+      )
+      .subscribe(() => {
+      this.isWindowSmall = window.innerWidth < 768;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next(0);
+    this.destroyed.complete();
   }
 
 }
