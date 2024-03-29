bf885b3bbb31864d7ed7c564c072b12fd3772c77
mdjeee <i.sejtniyazov@mail.ru>
2024-03-29 16:13:36 +0500

    fix: toast

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -4,11 +4,13 @@ import { CookieService } from '../services/cookie.service';
 import { environment } from '../../../environments/environment';
 import { AlertService } from '../services/alert.service';
 import { AlertType } from '../../shared/enums/alert.enum';
+import { ToggleService } from '../services/toggle.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const cookieService = inject(CookieService);
   const toastService = inject(AlertService);
+  const toggleService = inject(ToggleService);
 
   if(cookieService.get(environment.token)) {
     return true;
@@ -17,6 +19,7 @@ export const authGuard: CanActivateFn = (route, state) => {
       type: AlertType.danger,
       text: 'Unauthorized'
     })
+    toggleService.isOpen.next(false);
     router.navigateByUrl('/login');
     return false;
   }
